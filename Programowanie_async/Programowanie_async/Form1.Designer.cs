1d4f7ee02ae93047843d07eab6f94b488d4db74d
Michaelxdd <michal.intel15@gmail.com>
2016-01-11 00:51:41 +0100

    3 laborka

---
+++ Programowanie_async/Programowanie_async/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Programowanie_async
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda wsparcia projektanta - nie należy modyfikować
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(53, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(276, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wciśnij klawisz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(49, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pokaż programowanie asynchroniczne";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 319);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Programowanie_async/Programowanie_async/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programowanie_async
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            
+            moja2metoda();
+            label1.Text = "Czekam na odpowiedź..."; // Tekst, który pojawia się po wciśnięciu klawisza
+        }
+
+        private async void moja2metoda()
+        {
+            var result = await mojametodasync("Michał"); //2 wątek
+            label1.Text = result;
+        }
+        
+        private Task<string> mojametodasync(string name)
+        {
+            return Task.Factory.StartNew(() => mojametoda(name));
+        }
+        
+        private string mojametoda(string name) //1 wątek
+        {
+            Thread.Sleep(2500); // opóźnienie 2,5 sekundowe
+            return "Witaj " + name;
+        }
+    }
+}
